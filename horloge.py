6efe9f78299bad78ed74b6105f1478ab97fc99a9
Tim-ats-d <darkogm69@gmail.com>
2020-08-30 00:09:56 +0200

    Center the display of clock in terminal

--- horloge.py
+++ horloge.py
@@ -7,6 +7,15 @@ import os
 
 ROWS, COLS = os.get_terminal_size()
 
+def centrer(string: str):
+    """
+        Returns centering calculations depending on the length of string.
+    """
+
+    return int((ROWS//2) - (len(string)//2) - len(string)%2)
+
+
+
 def hour() -> str:
     """
         Returns a string containing current hour, minute and second.
@@ -44,15 +53,17 @@ def day_flow() -> str:
 
 
 
-def grid(width: int, length: int) -> str:
+def grid(length: int, width: int, centersize: int) -> str:
     """
-        Prints a grid in ASCII character with a width and length.
+        Prints a grid in ASCII character with length and width centersize centered to centersize.
     """
 
-    starting_line = f"╔{length*'═'}╗"
-    end_line = f"╚{length*'═'}╝"
+    starting_line = f"╔{length*'═'}╗".center(centersize)
+    end_line = f"╚{length*'═'}╝".center(centersize)
+    print(starting_line)
 
-    return "\n".join((starting_line, "\n".join((f"║{' '*length}║" for line in range(width))), end_line))
+    return "".join((starting_line, "".join((f"║{' '*length}║".center(centersize) for line in range(width))), end_line))
+
 
 
 
@@ -63,21 +74,24 @@ def screen(stdscr):
 
     stdscr.clear()
     curses.curs_set(0)
-    curses.resizeterm(12, 41)
+    curses.resizeterm(COLS, ROWS)
 
     curses.start_color()
     curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_WHITE)
+    curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_CYAN)
 
-    statusbar = "The time was given to you by Tim." .center(COLS, " ")
+    statusbar = "The time was given to you by Tim."
 
-    stdscr.addstr(0, 0, grid(5, 38))
-    stdscr.addstr(2, 15, date(), curses.color_pair(1))
-    stdscr.addstr(11, 0, statusbar.center(40), curses.color_pair(2))
+    start_x_hour = centrer(hour())
+    start_x_statusbar = centrer(statusbar)
+
+    stdscr.addstr(7, 0, grid(40, 5, ROWS))
+    stdscr.addstr(9, start_x_hour, date(), curses.color_pair(1))
+    stdscr.addstr(18, start_x_statusbar, statusbar, curses.color_pair(2))
 
     while 1:
-        stdscr.addstr(4, 15, hour(), curses.color_pair(1))
-        stdscr.addstr(8, 0, f"{day_flow()} % of the day elapsed.".center(40), curses.color_pair(1))
+        stdscr.addstr(11, start_x_hour, hour(), curses.color_pair(1))
+        stdscr.addstr(15, 0, f"{day_flow()} % of the day elapsed.".center(ROWS), curses.color_pair(1))
 
         stdscr.refresh()
         time.sleep(1)
@@ -86,4 +100,3 @@ def screen(stdscr):
 
 if __name__ == "__main__":
     curses.wrapper(screen)
-
